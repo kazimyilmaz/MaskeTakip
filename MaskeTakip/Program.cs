3603b2bc996b7c8cd0b406ae1d186c5fc155bec6
Kazim YILMAZ <kazimyillmaz@gmail.com>
2024-03-23 21:36:21 +0300

    Add project files.

---
+++ MaskeTakip/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+
+
+
+using Business.Concrete;
+using Entities.Concrete;
+
+SelamVer(isim: "Kazım");
+SelamVer();
+
+Person person1 = new Person();
+person1.FirstName = "KAZIM";
+person1.LastName = "YILMAZ";  
+person1.DateOfBirthYear = 2000;
+person1.NationalyIdentity = 123;
+
+static void SelamVer(string isim = "İsimsiz")
+{
+    Console.WriteLine(  "Merhaba" + isim);
+}
+
+PttManager pttManager = new(new PersonManager());
+pttManager.GiveMask(person1);   
+
+
+//Console.WriteLine("Hello, World!");
